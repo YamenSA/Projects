a23c48ac5481bac0a81e02aa5cfab83030da8b42
Yamen Sandakli <yamen.gst@gmail.com>
2025-01-06 12:42:41 +0100

    Basic methods of the calc. were developed

---
+++ Calculator/Calculator/src/calculator/Calculator.java
@@ -0,0 +1,45 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static Scanner input = new Scanner(System.in);
+
+	public static double add(double ersteZahl, double zweiteZahl) {
+
+		return ersteZahl + zweiteZahl;
+	}
+
+	public static double sub(double ersteZahl, double zweiteZahl) {
+		return ersteZahl - zweiteZahl;
+	}
+	
+	public static double multiply(double ersteZahl, double zweiteZahl) {
+		return ersteZahl*zweiteZahl;
+	}
+	
+	public static double divide(double ersteZahl, double zweiteZahl) {
+		
+		if (zweiteZahl==0) {
+			System.out.println("Teilen durch 0 ist nicht möglich");
+		}
+		
+		return ersteZahl/zweiteZahl;
+	}
+
+	public static void main(String[] args) {
+		
+		double ersteZahl = input.nextDouble();
+		double zweiteZahl = input.nextDouble();
+		
+		
+		System.out.println((int)add(ersteZahl,zweiteZahl));
+		System.out.println((int)sub(ersteZahl,zweiteZahl));
+		System.out.println((int)multiply(ersteZahl, zweiteZahl));
+		System.out.println((int)divide(ersteZahl,zweiteZahl));
+		
+	}
+	
+
+}
